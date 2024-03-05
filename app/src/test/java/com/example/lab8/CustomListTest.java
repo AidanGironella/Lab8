c7aad7f41fc7113c8e757efa9604136aa5276a6b
Aidan Gironella <aidangironella@gmail.com>
2024-03-05 13:53:52 -0700

    hasCityTest that should fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,10 +2,10 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -23,5 +23,15 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(), listSize+1);
+    }
+
+    @Test
+    public void hasCityTest() {
+        list = MockCityList();
+        City cityShouldExist = new City("Edmonton", "Alberta");
+        list.addCity(cityShouldExist);
+        assertTrue(list.hasCity(cityShouldExist));
+        City cityShouldNotExist = new City("Calgary", "Alberta");
+        assertFalse(list.hasCity(cityShouldNotExist));
     }
 }
